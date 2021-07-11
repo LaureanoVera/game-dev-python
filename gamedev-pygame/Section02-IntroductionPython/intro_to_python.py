a31edb3b6e8a8227d331ec71a00362f04903aa34
LaureanoVera <laureanoigvera@gmail.com>
2021-07-11 10:31:43 -0300

    exersice 1

--- gamedev-pygame/Section02-IntroductionPython/intro_to_python.py
+++ gamedev-pygame/Section02-IntroductionPython/intro_to_python.py
@@ -15,3 +15,15 @@ your_age = int(input('How old are you? '))
 birth_yaner = 2021 - your_age
 
 print(birth_yaner)
+
+# EXERSICE 1: Inputs and Logical Operations
+num_a = float(input('Number a: '))
+num_b = float(input('Number b: '))
+subtract = num_a - num_b
+
+print(f'Result: {subtract}')
+
+if subtract < 0:
+  print('Negative Number')
+else:
+  print('Positive Number')
