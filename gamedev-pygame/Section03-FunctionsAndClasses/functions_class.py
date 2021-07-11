b81727959dd40d4ff49b21a5685892b13ab3c797
LaureanoVera <laureanoigvera@gmail.com>
2021-07-11 14:11:25 -0300

    return functions

--- gamedev-pygame/Section03-FunctionsAndClasses/functions_class.py
+++ gamedev-pygame/Section03-FunctionsAndClasses/functions_class.py
@@ -6,3 +6,11 @@ def homework_task(lang):
 print('===== Start =====')
 homework_task('Python')
 print('===== End =====')
+
+# Return Functions
+def name_uppercase(name):
+  print(f'Hello {name}!')
+  return name.upper()
+
+name_res = name_uppercase('Laureano')
+print(name_res)
