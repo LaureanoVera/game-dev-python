692b89b4de5ff801060cfee1dd049ad4792a38a6
LaureanoVera <laureanoigvera@gmail.com>
2021-07-11 15:45:04 -0300

    naming and filling the game screen

--- gamedev-pygame/Section04-IntroductionPygame/intro_pygame.py
+++ gamedev-pygame/Section04-IntroductionPygame/intro_pygame.py
@@ -3,6 +3,7 @@ import pygame
 width, height = 800, 600
 
 GScreen = pygame.display.set_mode((width, height))
+pygame.display.set_caption('Game One')
 
 def main():
   run = True
@@ -11,6 +12,8 @@ def main():
     for event in pygame.event.get():
       if event.type == pygame.QUIT:
         pygame.quit()
+    GScreen.fill((0, 15, 0))
+    pygame.display.update()
 
 if __name__ == '__main__':
   main()
