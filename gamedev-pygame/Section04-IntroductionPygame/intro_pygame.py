ffeb0e4d17bb7a8e27a4ba9d96431411e008bcbd
LaureanoVera <laureanoigvera@gmail.com>
2021-07-12 15:54:38 -0300

    set screen fps

--- gamedev-pygame/Section04-IntroductionPygame/intro_pygame.py
+++ gamedev-pygame/Section04-IntroductionPygame/intro_pygame.py
@@ -2,18 +2,27 @@ import pygame
 
 width, height = 800, 600
 
+FPS = 60
+
 GScreen = pygame.display.set_mode((width, height))
 pygame.display.set_caption('Game One')
 
+def draw_fn():
+    GScreen.fill((0, 15, 0))
+    pygame.display.update()
+
 def main():
   run = True
+  clock = pygame.time.Clock()
 
   while run:
+    clock.tick(FPS)
     for event in pygame.event.get():
       if event.type == pygame.QUIT:
         pygame.quit()
-    GScreen.fill((0, 15, 0))
-    pygame.display.update()
+
+    draw_fn()
+
 
 if __name__ == '__main__':
   main()
