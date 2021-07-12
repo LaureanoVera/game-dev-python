075cc45dc0e1209a3e25533c28d262274f8383e8
LaureanoVera <laureanoigvera@gmail.com>
2021-07-12 16:11:35 -0300

    import images

--- gamedev-pygame/Section04-IntroductionPygame/intro_pygame.py
+++ gamedev-pygame/Section04-IntroductionPygame/intro_pygame.py
@@ -1,4 +1,5 @@
 import pygame
+import os
 
 width, height = 800, 600
 
@@ -6,23 +7,25 @@ FPS = 60
 
 GScreen = pygame.display.set_mode((width, height))
 pygame.display.set_caption('Game One')
+PICTURE = pygame.image.load(os.path.join('resources', 'char.png'))
 
 def draw_fn():
     GScreen.fill((0, 15, 0))
+    GScreen.blit(PICTURE, (0, 0))
     pygame.display.update()
 
+
 def main():
-  run = True
-  clock = pygame.time.Clock()
+    run = True
+    clock = pygame.time.Clock()
 
-  while run:
-    clock.tick(FPS)
-    for event in pygame.event.get():
-      if event.type == pygame.QUIT:
-        pygame.quit()
-
-    draw_fn()
+    while run:
+        clock.tick(FPS)
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+        draw_fn()
 
 
 if __name__ == '__main__':
-  main()
+    main()
