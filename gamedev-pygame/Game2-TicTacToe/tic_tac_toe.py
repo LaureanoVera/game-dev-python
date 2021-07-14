fd5c9b09c5de9b23467bdb861a8baa33bf7896ca
LaureanoVera <laureanoigvera@gmail.com>
2021-07-14 14:12:09 -0300

    set borders

--- gamedev-pygame/Game2-TicTacToe/tic_tac_toe.py
+++ gamedev-pygame/Game2-TicTacToe/tic_tac_toe.py
@@ -2,11 +2,26 @@ import pygame
 
 the_board = {'7': ' ', '8': ' ', '9': ' ',
              '4': ' ', '5': ' ', '6': ' ',
-             '1': ' ', '2': ' ', '3': ' ',}
+             '1': ' ', '2': ' ', '3': ' ', }
 
 board_keys = []
 
-print(board_keys)
-
+# print(board_keys)
 for key in the_board:
-  board_keys.append(key)
+    board_keys.append(key)
+# print(board_keys)
+
+
+def print_board(board):
+    print(board['7'] + '/' + board['8'] + '/' + board['9'])
+    print('-*-*-')
+
+    print(board['4'] + '/' + board['5'] + '/' + board['6'])
+    print('-*-*-')
+
+    print(board['1'] + '/' + board['2'] + '/' + board['3'])
+    print('-*-*-')
+
+print_board(the_board)
+
+print('### END ###')
