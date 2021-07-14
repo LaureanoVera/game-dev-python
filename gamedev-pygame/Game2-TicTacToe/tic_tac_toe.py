eecf268be7213e480aacab471f3deba879874ab0
LaureanoVera <laureanoigvera@gmail.com>
2021-07-14 10:48:54 -0300

    set up

---
+++ gamedev-pygame/Game2-TicTacToe/tic_tac_toe.py
@@ -0,0 +1,12 @@
+import pygame
+
+the_board = {'7': ' ', '8': ' ', '9': ' ',
+             '4': ' ', '5': ' ', '6': ' ',
+             '1': ' ', '2': ' ', '3': ' ',}
+
+board_keys = []
+
+print(board_keys)
+
+for key in the_board:
+  board_keys.append(key)
