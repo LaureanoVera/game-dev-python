9140ee862a377862c1ac15d67def0a80aef98e22
LaureanoVera <laureanoigvera@gmail.com>
2021-07-14 11:09:26 -0300

    move players

--- pong_game.py
+++ pong_game.py
@@ -12,16 +12,16 @@ player_one = turtle.Turtle()
 player_one.speed(0)
 player_one.shape('square')
 player_one.color('white')
-player_one.penup() # clean line
+player_one.penup()  # clean line
 player_one.goto(-350, 0)
-player_one.shapesize(stretch_wid=3, stretch_len=1)
+player_one.shapesize(stretch_wid=5, stretch_len=1)
 
 # PLAYER TWO
 player_two = turtle.Turtle()
 player_two.speed(0)
 player_two.shape('square')
 player_two.color('white')
-player_two.penup() # clean line
+player_two.penup()  # clean line
 player_two.goto(350, 0)
 player_two.shapesize(stretch_wid=5, stretch_len=1)
 
@@ -30,7 +30,7 @@ ball = turtle.Turtle()
 ball.speed(0)
 ball.shape('square')
 ball.color('white')
-ball.penup() # clean line
+ball.penup()  # clean line
 ball.goto(0, 0)
 
 # LINE
@@ -39,5 +39,34 @@ line.color('white')
 line.goto(0, -400)
 line.goto(0, 400)
 
+# FUNCTIONS
+def player_one_up():
+    y = player_one.ycor()
+    y += 20
+    player_one.sety(y)
+
+def player_one_down():
+    y = player_one.ycor()
+    y -= 20
+    player_one.sety(y)
+
+
+def player_two_up():
+  y = player_two.ycor()
+  y += 20
+  player_two.sety(y)
+
+def player_two_down():
+  y = player_two.ycor()
+  y -= 20
+  player_two.sety(y)
+
+# KEYS
+window.listen()
+window.onkeypress(player_one_up, 'w')
+window.onkeypress(player_one_down, 's')
+window.onkeypress(player_two_up, 'Up')
+window.onkeypress(player_two_down, 'Down')
+
 while True:
-  window.update()
+    window.update()
