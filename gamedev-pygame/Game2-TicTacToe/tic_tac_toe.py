ab16042a52594bcd85b85b82e8a1a3008fb807b2
LaureanoVera <laureanoigvera@gmail.com>
2021-07-14 14:35:19 -0300

    restart the game

--- gamedev-pygame/Game2-TicTacToe/tic_tac_toe.py
+++ gamedev-pygame/Game2-TicTacToe/tic_tac_toe.py
@@ -36,7 +36,7 @@ def game():
 
         move = input()
 
-        if the_board[move] == '':
+        if the_board[move] == ' ':
             the_board[move] == turn
             count += 1
         else:
@@ -45,52 +45,70 @@ def game():
             continue
 
         if count >= 5:
-          if the_board['7'] == the_board['8'] == the_board['9'] != ' ':
-            print_board(the_board)
-            print('\nGame Over\n')
-            print('Player '+ turn + 'won the game')
-            break
+            if the_board['7'] == the_board['8'] == the_board['9'] != ' ':
+                print_board(the_board)
+                print('\nGame Over\n')
+                print('Player ' + turn + 'won the game')
+                break
 
-          if the_board['4'] == the_board['5'] == the_board['6'] != ' ':
-            print_board(the_board)
-            print('\nGame Over\n')
-            print('Player '+ turn + 'won the game')
-            break
+            if the_board['4'] == the_board['5'] == the_board['6'] != ' ':
+                print_board(the_board)
+                print('\nGame Over\n')
+                print('Player ' + turn + 'won the game')
+                break
 
-          if the_board['1'] == the_board['2'] == the_board['3'] != ' ':
-            print_board(the_board)
-            print('\nGame Over\n')
-            print('Player '+ turn + 'won the game')
-            break
+            if the_board['1'] == the_board['2'] == the_board['3'] != ' ':
+                print_board(the_board)
+                print('\nGame Over\n')
+                print('Player ' + turn + 'won the game')
+                break
 
-          if the_board['1'] == the_board['4'] == the_board['7'] != ' ':
-            print_board(the_board)
-            print('\nGame Over\n')
-            print('Player '+ turn + 'won the game')
-            break
+            if the_board['1'] == the_board['4'] == the_board['7'] != ' ':
+                print_board(the_board)
+                print('\nGame Over\n')
+                print('Player ' + turn + 'won the game')
+                break
 
-          if the_board['2'] == the_board['5'] == the_board['8'] != ' ':
-            print_board(the_board)
-            print('\nGame Over\n')
-            print('Player '+ turn + 'won the game')
-            break
+            if the_board['2'] == the_board['5'] == the_board['8'] != ' ':
+                print_board(the_board)
+                print('\nGame Over\n')
+                print('Player ' + turn + 'won the game')
+                break
 
-          if the_board['3'] == the_board['6'] == the_board['9'] != ' ':
-            print_board(the_board)
-            print('\nGame Over\n')
-            print('Player '+ turn + 'won the game')
-            break
+            if the_board['3'] == the_board['6'] == the_board['9'] != ' ':
+                print_board(the_board)
+                print('\nGame Over\n')
+                print('Player ' + turn + 'won the game')
+                break
 
-          if the_board['1'] == the_board['5'] == the_board['9'] != ' ':
-            print_board(the_board)
-            print('\nGame Over\n')
-            print('Player '+ turn + 'won the game')
-            break
-            
-          if the_board['3'] == the_board['5'] == the_board['7'] != ' ':
-            print_board(the_board)
-            print('\nGame Over\n')
-            print('Player '+ turn + 'won the game')
-            break
+            if the_board['1'] == the_board['5'] == the_board['9'] != ' ':
+                print_board(the_board)
+                print('\nGame Over\n')
+                print('Player ' + turn + 'won the game')
+                break
 
-print('### END ###')
+            if the_board['3'] == the_board['5'] == the_board['7'] != ' ':
+                print_board(the_board)
+                print('\nGame Over\n')
+                print('Player ' + turn + 'won the game')
+                break
+
+        if count == 9:
+            print('\nGame Over\n')
+            print('The game is Tie')
+
+        if turn == 'X':
+            turn = 'O'
+        else:
+            turn = 'X'
+
+    restart = input('Do you want to restart the Game? (y/n)')
+
+    if restart == 'Y' or restart == 'y':
+        for key in board_keys:
+            the_board[key] = ' '
+        game()
+
+
+if __name__ == '__main__':
+    game()
