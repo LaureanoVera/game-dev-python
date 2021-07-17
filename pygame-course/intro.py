7fa68223bcb0b0eb984d7ff818e29aab2e067f09
LaureanoVera <laureanoigvera@gmail.com>
2021-07-17 16:46:15 -0300

    figures with for loops

--- pygame-course/intro.py
+++ pygame-course/intro.py
@@ -25,9 +25,12 @@ while run:
     screen.fill(WHITE)
 
     # ---- Draw Zone ----
-    pygame.draw.line(screen, GREEN, [0, 0], [800, 600], 3)
-    pygame.draw.rect(screen, BLUE, (100, 100, 40, 40))
-    pygame.draw.circle(screen, RED, (200, 200), 20)
+    for x in range(100, 700, 100):
+      pygame.draw.rect(screen, BLACK, (x, 275, 50, 50))
+      pygame.draw.line(screen, GREEN, (x, 0), (x, 100), 5)
+    # pygame.draw.line(screen, GREEN, [0, 0], [800, 600], 3)
+    # pygame.draw.rect(screen, BLUE, (x, y, tam, tam))
+    # pygame.draw.circle(screen, RED, (200, 200), 20)
     # ---- Draw Zone ----
 
     # Update Screen
