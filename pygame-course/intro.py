038717b52fc7b1d98770beb1edcc42079da93e8f
LaureanoVera <laureanoigvera@gmail.com>
2021-07-17 16:41:29 -0300

    dibujando figuras en la pantalla

--- pygame-course/intro.py
+++ pygame-course/intro.py
@@ -1,8 +1,15 @@
 import pygame
 import sys
-pygame.init()  # Initialize
+pygame.init()  # Initialize pygame
 
-size = (800, 500)
+# Colors
+BLACK = (0,   0,   0)
+WHITE = (255, 255, 255)
+RED = (255,   0,   0)
+GREEN = (0, 255,   0)
+BLUE = (0,   0, 255)
+
+size = (800, 600)
 
 # Create Window
 screen = pygame.display.set_mode(size)
@@ -11,6 +18,17 @@ screen = pygame.display.set_mode(size)
 run = True
 while run:
     for event in pygame.event.get():
-        print(event)
         if event.type == pygame.QUIT:
             sys.exit()
+
+    # BG Color
+    screen.fill(WHITE)
+
+    # ---- Draw Zone ----
+    pygame.draw.line(screen, GREEN, [0, 0], [800, 600], 3)
+    pygame.draw.rect(screen, BLUE, (100, 100, 40, 40))
+    pygame.draw.circle(screen, RED, (200, 200), 20)
+    # ---- Draw Zone ----
+
+    # Update Screen
+    pygame.display.flip()
