a85d17657e31737341b778f9a5887ccf129bb364
LaureanoVera <laureanoigvera@gmail.com>
2021-07-17 21:30:15 -0300

    mouse detection

--- the-game/game.py
+++ the-game/game.py
@@ -12,6 +12,13 @@ screen = pygame.display.set_mode(size)
 pygame.display.set_caption('The GAME?')
 clock = pygame.time.Clock()
 
+pygame.mouse.set_visible(0)
+
+# --- GAME VARS
+points = 0
+
+# 
+
 # --- GAME LOOP
 run = True
 while run:
@@ -20,8 +27,16 @@ while run:
       run = False
       sys.exit()
 
+  # --- MOUSE
+  mouse = pygame.mouse.get_pos()
+  x = mouse[0]
+  y = mouse[1]
+
   # --- BG COLOR
   screen.fill(dark)
+
+  # --- PLAYER
+  pygame.draw.circle(screen, blue, (x, y), 20)
 
   # --- UPDATE
   pygame.display.flip()
