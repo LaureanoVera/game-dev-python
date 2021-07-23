3863fb77bef5924d035664210b5f6e6d1e22b5a5
LaureanoVera <laureanoigvera@gmail.com>
2021-07-23 08:27:47 -0300

    configuration

---
+++ gamedev-pygame/Game3-PokemonPuzzle/resources/main.py
@@ -0,0 +1,13 @@
+import os
+
+SCREEN_SIZE = (640, 640)
+PRICTURE_ROOT_DIR = os.path.join(os.getcwd(), 'pictures')
+FONTPATH = os.path.join(os.getcwd(), 'font/FZSTK.TTF')
+
+BACKGROUNDCOLOR = (255, 255, 255)
+RED = (255, 0, 0)
+GREEN = (0, 255, 0)
+BLUE = (0, 0, 255)
+
+FPS = 40
+RANDNUM = 100
