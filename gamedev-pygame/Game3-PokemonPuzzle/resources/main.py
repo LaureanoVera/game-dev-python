532b872a8e0760acafb5bcfb0b06227e949da124
LaureanoVera <laureanoigvera@gmail.com>
2021-07-23 08:37:15 -0300

    move parts game

--- gamedev-pygame/Game3-PokemonPuzzle/resources/main.py
+++ gamedev-pygame/Game3-PokemonPuzzle/resources/main.py
@@ -1,13 +1,25 @@
 import os
+import sys
+import cfg
+import random
+import pygame
 
-SCREEN_SIZE = (640, 640)
-PRICTURE_ROOT_DIR = os.path.join(os.getcwd(), 'pictures')
-FONTPATH = os.path.join(os.getcwd(), 'font/FZSTK.TTF')
+def is_game_over(board, size):
+  assert is_instance(size, int)
+  num_cells = size * size
+  for i in range(num_cells - 1):
+    if board[i] != i: False
+  return True
 
-BACKGROUNDCOLOR = (255, 255, 255)
-RED = (255, 0, 0)
-GREEN = (0, 255, 0)
-BLUE = (0, 0, 255)
+def move_r(board, blank_cell_idx, num_cols):
+  if blank_cell_idx % num_cols == 0: return blank_cell_idx
+  board[blank_cell_idx-1], board[blank_cell_idx] = board[blank_cell_idx], board[blank_cell_idx-1]
+  return blank_cell_idx-1
 
-FPS = 40
-RANDNUM = 100
+def move_l(board, blank_cell_idx, num_cols):
+  if (blank_cell_idx+1) % num_cols == 0: return blank_cell_idx
+  board[blank_cell_id+1], board[blank_cell_idx] = board[blank_cell_idx], board[blank_cell_idx+1]
+  return blank_cell_idx+1
+
+if __name__ == '__main__':
+    main()
