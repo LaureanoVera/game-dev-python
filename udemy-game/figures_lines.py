6105dac9ad02bcbc80f312de9f341def4deffb57
LaureanoVera <laureanoigvera@gmail.com>
2021-09-26 10:19:48 -0300

    udemy create pygame game: create circles with pygame

--- udemy-game/figures_lines.py
+++ udemy-game/figures_lines.py
@@ -7,18 +7,26 @@ from pygame.locals import *
 pygame.init()
 
 # SCREEN CONFIG
-SIZE = (400, 300)
+SIZE = (800, 600)
 screen = pygame.display.set_mode(SIZE)
 
 # SCREEN STYLES
 pygame.display.set_caption('Game Title')
-bg_color = (1, 150, 70)
-line_color = (182, 153, 5)
+bg_color = (33, 29, 44)
+line_color = (0, 255, 255)
+circle_color = (1, 150, 70)
+figure_color = (255, 0, 255)
 
 # GAME LOOP
 while True:
   screen.fill(bg_color)
-  pygame.draw.line(screen, line_color, (20, 30), (140, 100), 10)
+  # LINES
+  pygame.draw.line(screen, line_color, (60, 30), (140, 100), 5)
+  pygame.draw.line(screen, line_color, (80, 190), (140, 100), 5)
+  pygame.draw.line(screen, line_color, (10, 70), (140, 100), 5)
+  # CIRCLES
+  pygame.draw.circle(screen, circle_color, (400, 280), 60, 10)
+  pygame.draw.circle(screen, circle_color, (120, 530), 40, 5)
   for event in pygame.event.get():
     if event.type == QUIT:
       # QUIT GAME
