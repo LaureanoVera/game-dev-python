962c58df63b472602df8ce5ea03679a94ef750dd
LaureanoVera <laureanoigvera@gmail.com>
2021-09-26 10:15:05 -0300

    udemy create pygame game: create line with pygame

--- udemy-game/figures_lines.py
+++ udemy-game/figures_lines.py
@@ -13,10 +13,12 @@ screen = pygame.display.set_mode(SIZE)
 # SCREEN STYLES
 pygame.display.set_caption('Game Title')
 bg_color = (1, 150, 70)
+line_color = (182, 153, 5)
 
 # GAME LOOP
 while True:
   screen.fill(bg_color)
+  pygame.draw.line(screen, line_color, (20, 30), (140, 100), 10)
   for event in pygame.event.get():
     if event.type == QUIT:
       # QUIT GAME
